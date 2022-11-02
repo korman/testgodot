723fd122feef43f550665a46f8985bac22b220fc
Korman <korman@qq.com>
2022-11-02 16:09:49 +0800

    Added base animated node

--- PlayerSprite.gd
+++ PlayerSprite.gd
@@ -8,9 +8,14 @@ extends AnimatedSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("im ready")
+	self.play("walk_right")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_PlayerSprite_animation_finished():
+	self.flip_h = !self.flip_h
