efbfdf40464d22e5e1c563ce28afb85d81002e71
Korman <korman@qq.com>
2022-11-03 09:57:22 +0800

    init

--- PlayerSprite.gd
+++ PlayerSprite.gd
@@ -8,7 +8,7 @@ extends AnimatedSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.play("walk_right")
+	self.stop()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,14 +17,27 @@ func _ready():
 
 
 func _on_PlayerSprite_animation_finished():
-	self.flip_h = !self.flip_h
+	pass
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.scancode == KEY_W:
 			self.play("walk_up")
+			self.position = self.position + Vector2(0,-10)
 		
 		if event.scancode == KEY_S:
 			self.play("walk_down")
+			self.position = self.position + Vector2(0,10)
 
-	if event is InputEventKey and event.press
+		if event.scancode == KEY_A:
+			self.play("walk_right")
+			self.flip_h = true
+			self.position = self.position + Vector2(-10,0)
+
+		if event.scancode == KEY_D:
+			self.play("walk_right")
+			self.flip_h = false
+			self.position = self.position + Vector2(10,0)
+
+	if event is InputEventKey and !event.pressed:
+		self.stop()
